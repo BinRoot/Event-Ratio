0c11657154bf6f8a8d738eedeb73b6c6f164c255
octopi <octopie@gmail.com>
2012-07-21 18:16:26 -0400

    sample json

--- app.js
+++ app.js
@@ -4,10 +4,18 @@ var app = express.createServer(express.logger());
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('sup event handler');
+	response.send('sup event handler');
+});
+
+app.get('/test', function(req, res) {
+	res.send('testing');
+});
+
+app.get('/fetch', function(req, res) {
+
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+	console.log("Listening on " + port);
 });
