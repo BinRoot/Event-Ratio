71e0969bc3cdf97a96a517d48eec96610a9e0450
octopi <octopie@gmail.com>
2012-07-21 17:12:15 -0400

    reworking app.js for heroku

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-var app = require('express').createServer();
+var express = require('express');
 
-app.get('/', function(req, res){
-  res.send('sup event handler');
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  response.send('sup event handler');
 });
 
-app.listen(3000);
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
