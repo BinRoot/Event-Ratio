7abf1847748ed364197fe1dae0a9a4fbe0044e9e
octopi <octopie@gmail.com>
2012-07-21 17:33:05 -0400

    added public folder

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
   response.send('sup event handler');
