207efd8dac4cd59cb462d337b3eb7d9ca816a24e
octopi <octopie@gmail.com>
2012-07-22 02:21:50 -0400

    access token changed up, ages info included

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var http = require('https');
 var qs = require('querystring');
-var access_token = '';
 
 var HEROKU_URL = 'http://aqueous-cove-9179.herokuapp.com/allevents';
 var LOCAL_URL = 'http://localhost:5000/allevents';
@@ -14,66 +13,76 @@ app.get('/', function(request, response) {
 });
 
 app.get('/allevents', function(request, response) {
-	var code = request.query['code'];	
-	var fbPath = '/oauth/access_token?' + 
-   				'client_id=453762924657294' +
-   				'&redirect_uri=' + HEROKU_URL +
- 				'&client_secret=6c7d0f487d6b8916552a2d890d776e48' +
- 				'&code=' + code;
-	var options = {
-	    host: 'graph.facebook.com',
-	    port: 443,
-	    path: fbPath
-	};
+	var token_query = request.query['access_token'];
+	console.log('token_query is '+token_query);
+	if(token_query === undefined) {
+		var code = request.query['code'];	
+		var fbPath = '/oauth/access_token?' + 
+	   				'client_id=453762924657294' +
+	   				'&redirect_uri=' + HEROKU_URL +
+	 				'&client_secret=6c7d0f487d6b8916552a2d890d776e48' +
+	 				'&code=' + code;
+		var options = {
+		    host: 'graph.facebook.com',
+		    port: 443,
+		    path: fbPath
+		};
 
-	// get access token from FB
-	http.request(options, function(res) {
-		console.log('response is '+res.statusCode);
-		var str = '';
-		res.on('data', function(chunk) {
-			str += chunk;
-		});
-		res.on('end', function() {
-			var returns = qs.parse(str);
-			access_token = returns['access_token'];
-			
-			// actually grab all events from fb graph
-			var graphOptions = {
-				host: 'graph.facebook.com',
-				port: 443,
-				path: '/me/events?access_token=' + access_token
-			};
-			http.request(graphOptions, function(graphRes) {
-				var graphResult = '';
-				graphRes.on('data', function(chunk) {
-					graphResult += chunk;
-				});
-
-				graphRes.on('end', function() {
-					console.log('graph result ended, result is '+graphResult);
-					var events = JSON.parse(graphResult).data;
-					var eventsResponse = { 'events': [] };
-
-					for(var i = 0; i < events.length; i++) {
-						eventsResponse['events'].push(events[i].id);
-					}
-					response.send(eventsResponse);
-				});
-			}).end();
-		});
-	}).end();
+		// get access token from FB
+		http.request(options, function(res) {
+			var str = '';
+			res.on('data', function(chunk) {
+				str += chunk;
+			});
+			res.on('end', function() {
+				var returns = qs.parse(str);
+				var access_token = returns['access_token'];
+				getEvents(access_token, response);
+			});
+		}).end();
+	} else {
+		getEvents(token_query, response);
+	}
 });
 
+function getEvents(access_token, response) {
+	// actually grab all events from fb graph
+	var graphOptions = {
+		host: 'graph.facebook.com',
+		port: 443,
+		path: '/me/events?access_token=' + access_token
+	};
+	console.log('access token in getEvents is '+access_token);
+	http.request(graphOptions, function(graphRes) {
+		var graphResult = '';
+		graphRes.on('data', function(chunk) {
+			graphResult += chunk;
+		});
+
+		graphRes.on('end', function() {
+			console.log('graph result ended, result is '+graphResult);
+			var events = JSON.parse(graphResult).data;
+			var eventsResponse = { 'events': [] };
+
+			for(var i = 0; i < events.length; i++) {
+				eventsResponse['events'].push(events[i].id);
+			}
+			response.send(eventsResponse);
+		});
+	}).end();
+}
+
 app.get('/event/:id', function(request, response) {
-	console.log('inside event, access_token is '+access_token);
 	var id = request.params['id'];
+	var access_token = request.query['access_token'];
 	var output = {};
 
 	// FQL query
 	var fqlQueries = {
 		'rsvp_query': 'SELECT uid, rsvp_status FROM event_member WHERE eid=' + id,
-		'gender_query': 'SELECT sex FROM user WHERE uid IN (SELECT uid FROM #rsvp_query)',
-		'basic_info_query': 'SELECT name, start_time, location FROM event WHERE eid=' + id
+		'person_info_query': 'SELECT sex, birthday_date FROM user WHERE uid IN (SELECT uid FROM #rsvp_query WHERE rsvp_status = \'attending\')',
+		'basic_info_query': 'SELECT name, start_time, location FROM event WHERE eid=' + id,
+		'mutuals_query': 'SELECT uid1, uid2 FROM friend WHERE uid1 = me() AND uid2 IN (SELECT uid FROM #rsvp_query WHERE rsvp_status = \'attending\')'
 	};
 	var eventReqOption = {
 		host: 'graph.facebook.com',
@@ -92,7 +101,8 @@ app.get('/event/:id', function(request, response) {
 			var data = JSON.parse(result).data;
 
 			var name, time, location;
-			var attending = 0, maybe = 0, declined = 0, invited = 0, male = 0, female = 0;
+			var attending = 0, maybe = 0, declined = 0, invited = 0, male = 0, female = 0, mutuals = 0;
+			var ages = [];
 
 			for(var k = 0; k < data.length; k++) {
 				var currResultsName = data[k].name;
@@ -110,14 +120,26 @@ app.get('/event/:id', function(request, response) {
 						}
 						invited++;
 					}
-				} else if(currResultsName === 'gender_query') {
-					// male/female 
+				} else if(currResultsName === 'person_info_query') {
+					// male/female/age
 					for(var i = 0; i < currResults.length; i++) {
-						var gender = currResults[i].sex;
+						var currUser = currResults[i];
+						var gender = currUser['sex'];
 						if(gender === 'male') {
 							male++;
 						} else if(gender === 'female') {
 							female++;
+						}
+
+						var birthdate = currUser['birthday_date'];
+						if(birthdate !== null) {
+							var bdaySplit = birthdate.split('/');
+							if(bdaySplit.length === 3) {
+								var thenDate = new Date(bdaySplit[2], bdaySplit[0], bdaySplit[1]);
+								var nowDate = Date.now();
+								var age = Math.floor((nowDate - thenDate) / 31557600000);
+								ages.push(age);
+							}
 						}
 					}
 				} else if(currResultsName === 'basic_info_query') {
@@ -125,8 +147,17 @@ app.get('/event/:id', function(request, response) {
 					name = currResults[0]['name'];
 					time = currResults[0]['start_time'];
 					location = currResults[0]['location'];
+				} else if(currResultsName === 'mutuals_query') {
+					mutuals = currResults.length;
 				}
 			}
+
+			// average out age
+			var averageAge = 0;
+			for(var i = 0; i < ages.length; i++) {
+				averageAge += ages[i];
+			}
+			averageAge = Math.round(averageAge / ages.length);
 
 			// send final result object
 			response.send({
@@ -138,8 +169,13 @@ app.get('/event/:id', function(request, response) {
 				'invited': invited,
 				'attending': attending,
 				'maybe': maybe,
-				'declined': declined
+				'declined': declined,
+				'mutuals': mutuals,
+				'ages': ages,
+				'averageAge': averageAge
 			});
+
+			// response.send(result);
 		})
 	}).end();
 
