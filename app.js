d6e3510347393eb9da0aa85f5832c91d30a6d4a6
octopi <octopie@gmail.com>
2012-07-22 03:58:42 -0400

    data null check, location returns empty string not null

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var http = require('https');
 var qs = require('querystring');
 
+
 var HEROKU_URL = 'http://aqueous-cove-9179.herokuapp.com/allevents';
 var LOCAL_URL = 'http://localhost:5000/allevents';
 
@@ -102,212 +103,213 @@ app.get('/event/:id', function(request, response) {
 		res.on('end', function() {
 			console.log('fqlresult is '+result);
 			var data = JSON.parse(result).data;
+			if(data !== null) {
 
-			var name, time, location;
-			var attending = 0, maybe = 0, declined = 0, invited = 0, male = 0, female = 0, mutuals = 0, numSingle = 0, numRelationship = 0, myAge;
-			var ages = [];
-
-			for(var k = 0; k < data.length; k++) {
-				var currResultsName = data[k].name;
-				var currResults = data[k].fql_result_set;
-				if(currResultsName === 'rsvp_query') {
-					// invited/attending/maybe
-					for(var i = 0; i < currResults.length; i++) {
-						var currResponse = currResults[i].rsvp_status;
-						if(currResponse === 'attending') {
-							attending++;
-						} else if(currResponse === 'unsure') {
-							maybe++;
-						} else if(currResponse === 'declined') {
-							declined++;
+				var name, time, location;
+				var attending = 0, maybe = 0, declined = 0, invited = 0, male = 0, female = 0, mutuals = 0, numSingle = 0, numRelationship = 0, myAge;
+				var ages = [];
+				for(var k = 0; k < data.length; k++) {
+					var currResultsName = data[k].name;
+					var currResults = data[k].fql_result_set;
+					if(currResultsName === 'rsvp_query') {
+						// invited/attending/maybe
+						for(var i = 0; i < currResults.length; i++) {
+							var currResponse = currResults[i].rsvp_status;
+							if(currResponse === 'attending') {
+								attending++;
+							} else if(currResponse === 'unsure') {
+								maybe++;
+							} else if(currResponse === 'declined') {
+								declined++;
+							}
+							invited++;
 						}
-						invited++;
+					} else if(currResultsName === 'person_info_query') {
+						// male/female/age
+						for(var i = 0; i < currResults.length; i++) {
+							var currUser = currResults[i];
+							var gender = currUser['sex'];
+							if(gender === 'male') {
+								male++;
+							} else if(gender === 'female') {
+								female++;
+							}
+
+							var age = calcAge(currUser['birthday_date']);
+							if(age !== null) {
+								ages.push(age);
+							}
+
+							var relationship = currUser['relationship_status'];
+							if(relationship !== null && relationship === 'Single') {
+								numSingle++;
+							} else if(relationship !== null && relationship !== 'Single') {
+								numRelationship++;
+							}
+						}
+					} else if(currResultsName === 'basic_info_query') {
+						// basic info
+						name = currResults[0]['name'];
+						time = currResults[0]['start_time'];
+						location = currResults[0]['location'];
+					} else if(currResultsName === 'mutuals_query') {
+						// mutuals
+						mutuals = currResults.length;
+					} else if(currResultsName === 'my_info_query') {
+						// my info
+						var myAge = calcAge(currResults[0]['birthday_date']);
 					}
-				} else if(currResultsName === 'person_info_query') {
-					// male/female/age
-					for(var i = 0; i < currResults.length; i++) {
-						var currUser = currResults[i];
-						var gender = currUser['sex'];
-						if(gender === 'male') {
-							male++;
-						} else if(gender === 'female') {
-							female++;
-						}
+				}
 
-						var age = calcAge(currUser['birthday_date']);
-						if(age !== null) {
-							ages.push(age);
-						}
-
-						var relationship = currUser['relationship_status'];
-						if(relationship !== null && relationship === 'Single') {
-							numSingle++;
-						} else if(relationship !== null && relationship !== 'Single') {
-							numRelationship++;
-						}
+				// average out age
+				var averageAge = 0;
+				if(ages.length > 0) {
+					for(var i = 0; i < ages.length; i++) {
+						averageAge += ages[i];
 					}
-				} else if(currResultsName === 'basic_info_query') {
-					// basic info
-					name = currResults[0]['name'];
-					time = currResults[0]['start_time'];
-					location = currResults[0]['location'];
-				} else if(currResultsName === 'mutuals_query') {
-					// mutuals
-					mutuals = currResults.length;
-				} else if(currResultsName === 'my_info_query') {
-					// my info
-					var myAge = calcAge(currResults[0]['birthday_date']);
+					averageAge = Math.round(averageAge / ages.length);
 				}
-			}
 
-			// average out age
-			var averageAge = 0;
-			if(ages.length > 0) {
-				for(var i = 0; i < ages.length; i++) {
-					averageAge += ages[i];
+				/*** BADGES ***/
+				var badges = [];
+
+				// gender ratio
+				var genderRatio = female/male;
+				if(genderRatio < .5) {
+					badges.push({
+						'id': 'sausageFest',
+						'name': 'Sausagefest',
+						'description': 'Do you work at a tech company? There are '+male+' dudes and only '+female+' chicks at this event.'
+					});
+				} else if(genderRatio > 1.5) {
+					badges.push({
+						'id': 'girlsNight',
+						'name': 'Girl\'s Night!',
+						'description': 'All the single ladies... now put your hands up! There are '+female+' girls and only '+male+' boys coming to this event.'
+					});
 				}
-				averageAge = Math.round(averageAge / ages.length);
-			}
 
-			/*** BADGES ***/
-			var badges = [];
+				// cougar/pedobar
+				if(myAge !== null && averageAge !== 0) {
+					var ageDiff = averageAge - myAge;
+					if(ageDiff > 3) {
+						badges.push({
+							'id': 'cougar',
+							'name': 'Cougar Alert!',
+							'description': 'Be on the lookout! The average age at this event is ' + ageDiff + ' years more than your age.'
+						});
+					} else if(ageDiff < -3) {
+						badges.push({
+							'id': 'pedobear',
+							'name': 'Pedobear',
+							'description': 'Why don\'t you take a seat right over there? The average age at this event is ' + ageDiff + ' years younger than you.'
+						});
+					}
+				}
 
-			// gender ratio
-			var genderRatio = female/male;
-			if(genderRatio < .5) {
-				badges.push({
-					'id': 'sausageFest',
-					'name': 'Sausagefest',
-					'description': 'Do you work at a tech company? There are '+male+' dudes and only '+female+' chicks at this event.'
+				// social butterfly/SAP
+				if(attending !== 0) {
+					var mutualRatio = mutuals/attending;
+					if(mutualRatio > .75) {
+						badges.push({
+							'id': 'socialButterfly',
+							'name': 'Social Butterfly',
+							'description': 'Are\'t you gonna be popular! ' + Math.floor(mutualRatio * 100) + '% of the attendees are your friends.'
+						});
+					} else if(mutualRatio < .25) {
+						badges.push({
+							'id': 'SAP',
+							'name': 'Socially Awkward Penguin',
+							'description': 'Get your one-liners ready. You\'re only friends with ' + Math.floor(mutualRatio * 100) + '% of the attendees.'
+						});
+					}
+				}
+
+				// intimate/rager
+				if(invited <= 15) {
+					badges.push({
+						'id': 'intimiateGathering',
+						'name': 'Intimiate Gathering',
+						'description': 'Sometimes you just gotta get away from the hubbub. There are only ' + invited + ' guests invited to this event.'
+					});
+				} else if(invited > 200) {
+					badges.push({
+						'id': 'rager',
+						'name': 'Rager',
+						'description': 'Get ready to rage! There are ' + invited + ' people invited to this event!'
+					});
+				}
+
+				// maybe/anyone home/no show
+				if(invited !== 0) {
+					var maybeRatio = maybe/invited;
+					if(maybeRatio > .5) {
+						badges.push({
+							'id': 'attendingMaybe',
+							'name': 'Attending Maybe?',
+							'description': 'Hey I just met you, and this is crazy... But here\'s my event, some come to it maybe? ' + Math.floor(maybeRatio * 100) + '% of those invited have responded with "Maybe."'
+						});
+					}
+
+					var responseRatio = (attending + declined + maybe) / invited;
+					if(responseRatio < .25) {
+						badges.push({
+							'id': 'anyoneHome',
+							'name': 'Anyone Home?',
+							'description': 'Is anybody there? Only ' + Math.floor(responseRatio * 100) + '% of invited guests have responded to the invite.'
+						});
+					}
+
+					var declineRatio = declined/invited;
+					if(declineRatio > .5) {
+						badges.push({
+							'id': 'noShow',
+							'name': 'No Show',
+							'description': Math.floor(declineRatio * 100) + '% of invitees have declined this event :('
+						});
+					}
+				}
+
+				// success kid (single) / third wheel
+				if(invited !== 0) {
+					var singleRatio = numSingle / invited;
+					if(singleRatio > .5) {
+						badges.push({
+							'id': 'successKid',
+							'name': 'Success Kid',
+							'description': Math.floor(singleRatio * 100) + '% of invitees are single.'
+						});
+					}
+					if(numRelationship > .5) {
+						badges.push({
+							'id': 'thirdWheel',
+							'name': 'Third Wheel',
+							'description': 'Third wheel much? ' + numRelationship + ' of the guests are not single.'
+						});
+					}
+				}
+
+
+				// send final result object
+				response.send({
+					'name': name,
+					'time': time,
+					'location': (location === null ? '' : location),
+					'male': male,
+					'female': female,
+					'invited': invited,
+					'attending': attending,
+					'maybe': maybe,
+					'declined': declined,
+					'mutuals': mutuals,
+					'ages': ages,
+					'averageAge': averageAge,
+					'badges': badges
 				});
-			} else if(genderRatio > 1.5) {
-				badges.push({
-					'id': 'girlsNight',
-					'name': 'Girl\'s Night!',
-					'description': 'All the single ladies... now put your hands up! There are '+female+' girls and only '+male+' boys coming to this event.'
-				});
-			}
 
-			// cougar/pedobar
-			if(myAge !== null && averageAge !== 0) {
-				var ageDiff = averageAge - myAge;
-				if(ageDiff > 3) {
-					badges.push({
-						'id': 'cougar',
-						'name': 'Cougar Alert!',
-						'description': 'Be on the lookout! The average age at this event is ' + ageDiff + ' years more than your age.'
-					});
-				} else if(ageDiff < -3) {
-					badges.push({
-						'id': 'pedobear',
-						'name': 'Pedobear',
-						'description': 'Why don\'t you take a seat right over there? The average age at this event is ' + ageDiff + ' years younger than you.'
-					});
-				}
-			}
-
-			// social butterfly/SAP
-			if(attending !== 0) {
-				var mutualRatio = mutuals/attending;
-				if(mutualRatio > .75) {
-					badges.push({
-						'id': 'socialButterfly',
-						'name': 'Social Butterfly',
-						'description': 'Are\'t you gonna be popular! ' + Math.floor(mutualRatio * 100) + '% of the attendees are your friends.'
-					});
-				} else if(mutualRatio < .25) {
-					badges.push({
-						'id': 'SAP',
-						'name': 'Socially Awkward Penguin',
-						'description': 'Get your one-liners ready. You\'re only friends with ' + Math.floor(mutualRatio * 100) + '% of the attendees.'
-					});
-				}
-			}
-
-			// intimate/rager
-			if(invited <= 15) {
-				badges.push({
-					'id': 'intimiateGathering',
-					'name': 'Intimiate Gathering',
-					'description': 'Sometimes you just gotta get away from the hubbub. There are only ' + invited + ' guests invited to this event.'
-				});
-			} else if(invited > 200) {
-				badges.push({
-					'id': 'rager',
-					'name': 'Rager',
-					'description': 'Get ready to rage! There are ' + invited + ' people invited to this event!'
-				});
-			}
-
-			// maybe/anyone home/no show
-			if(invited !== 0) {
-				var maybeRatio = maybe/invited;
-				if(maybeRatio > .5) {
-					badges.push({
-						'id': 'attendingMaybe',
-						'name': 'Attending Maybe?',
-						'description': 'Hey I just met you, and this is crazy... But here\'s my event, some come to it maybe? ' + Math.floor(maybeRatio * 100) + '% of those invited have responded with "Maybe."'
-					});
-				}
-
-				var responseRatio = (attending + declined + maybe) / invited;
-				if(responseRatio < .25) {
-					badges.push({
-						'id': 'anyoneHome',
-						'name': 'Anyone Home?',
-						'description': 'Is anybody there? Only ' + Math.floor(responseRatio * 100) + '% of invited guests have responded to the invite.'
-					});
-				}
-
-				var declineRatio = declined/invited;
-				if(declineRatio > .5) {
-					badges.push({
-						'id': 'noShow',
-						'name': 'No Show',
-						'description': Math.floor(declineRatio * 100) + '% of invitees have declined this event :('
-					});
-				}
-			}
-
-			// success kid (single) / third wheel
-			if(invited !== 0) {
-				var singleRatio = numSingle / invited;
-				if(singleRatio > .5) {
-					badges.push({
-						'id': 'successKid',
-						'name': 'Success Kid',
-						'description': Math.floor(singleRatio * 100) + '% of invitees are single.'
-					});
-				}
-				if(numRelationship > .5) {
-					badges.push({
-						'id': 'thirdWheel',
-						'name': 'Third Wheel',
-						'description': 'Third wheel much? ' + numRelationship + ' of the guests are not single.'
-					});
-				}
-			}
-
-
-			// send final result object
-			response.send({
-				'name': name,
-				'time': time,
-				'location': location,
-				'male': male,
-				'female': female,
-				'invited': invited,
-				'attending': attending,
-				'maybe': maybe,
-				'declined': declined,
-				'mutuals': mutuals,
-				'ages': ages,
-				'averageAge': averageAge,
-				'badges': badges
-			});
-
-			// response.send(result);
-		})
+				// response.send(result);
+			} // end data null check
+		}); // end res.on('end')
 	}).end();
 
 });
@@ -327,8 +329,9 @@ function calcAge(birthdate) {
 	return null;
 }
 
-app.get('/fetch', function(req, res) {
-
+// public view of event w/ this eid. makes calls to our own API
+app.get('/e/:eid', function(req, res) {
+	res.render('e.ejs', {'test': 'data'});
 });
 
 var port = process.env.PORT || 5000;
